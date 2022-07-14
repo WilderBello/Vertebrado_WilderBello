05bee1691062f726ef4645586eaf1dfa93490c4f
WilderBello <wobelloh@unal.edu.co>
2022-07-13 20:10:35 -0500

    Agregado Perro

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -5,6 +5,7 @@ public class GeneradorVertebrados {
         miVertebrados[1] = new Vertebrado("Vaca", "Mamífero");
         miVertebrados[2] = new Vertebrado("Tiburón", "Pez");
         miVertebrados[3] = new Vertebrado("Paloma", "Ave");
+        miVertebrados[3] = new Vertebrado("Perro", "Mamífero");
 
         return miVertebrados;
     }
