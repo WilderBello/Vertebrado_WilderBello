f7f1ec845e55565629ffa19eea108f465f904000
WilderBello <wobelloh@unal.edu.co>
2022-07-13 20:01:55 -0500

    Add acentos

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,8 +1,8 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados() {
         Vertebrado[] miVertebrados = new Vertebrado[4];
-        miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
+        miVertebrados[0] = new Vertebrado("Camello", "Mamífero");
+        miVertebrados[1] = new Vertebrado("Vaca", "Mamífero");
         miVertebrados[2] = new Vertebrado("Tiburón", "Pez");
         miVertebrados[2] = new Vertebrado("Paloma", "Ave");
 
